7720cd934891efa9fa0c6bb494af33583eb5b913
everlastingflame <charlesbooth1@icloud.com>
2024-05-04 20:44:00 -0400

    Corrected console.log and finalized script tags in html

--- routes/draft.js
+++ routes/draft.js
@@ -92,8 +92,6 @@ router.get("/:id/settings", async (req, res) => {
     }
 }).post("/:id/settings", async (req, res) => {
 
-    console.log(req.body)
-
     if(!req.body) return res.status(400).send("Need to invite a player to the draft");
     let body = req.body;
     try {
