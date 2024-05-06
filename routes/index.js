fa472423d6836021646ad1e797d0e5b2abedfbd5
Matt Turner <mdturner1026@gmail.com>
2024-05-06 11:54:20 -0400

    Removed test websocket files

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ import tournaments from './tournaments.js';
 const constructorMethod = (app) => {
   app.use('/', users);
   app.use('/draft', draft);
-  app.use('/draft/:id/ws', express.Router().get("/", async(req, res) => {
-    res.render("ws-test")
-  }))
   app.use('/tournament', tournaments);
   app.use('*', (req, res) => {
     return res.status(404).render("userError", {layout: 'userProfiles', title: 'Error', notFound: true});
